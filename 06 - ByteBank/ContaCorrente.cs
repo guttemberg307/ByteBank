feb403725392c8bf1d08fe273814e20a5c2b1069
Gutemberg Nascimento da Silva <iblue.gnsilva@terceiros.mag.com.br>
2022-01-14 22:08:43 -0300

    02 - ObterSaldo e DefinirSaldo

---
+++ 06 - ByteBank/ContaCorrente.cs
@@ -0,0 +1,59 @@
+﻿
+//using _05_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente titular; 
+        public int agencia;
+        public int numero;
+        private double saldo = 100; // --> valor padrão
+
+
+        public void DefinirSaldo(double saldo)
+        {
+           if(saldo < 0) //--> se o saldo for negativo nao vai retornar valor algum 
+            {
+                return;
+            }
+              this.saldo = saldo;  //--> retornona o valor atual caso seja positivo 
+        }
+
+        public double ObterSaldo()
+        {
+            return saldo; //--> somente obtem o saldo atual da conta 
+        }
+
+
+        public bool Sacar(double valor) 
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            this.saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor) 
+
+        {
+            saldo += valor; 
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo < valor) 
+            {
+                return false; 
+            }
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+    }
+}
